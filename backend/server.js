49876a019aeafaa9741506aede12fc8bb5c5bada
Sahil Shah <sahilshah2104@gmail.com>
2024-06-09 17:58:49 +0530

    Feat: Production Deployment Changes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 const { app, server } = require('./socket/socket');
 const authRouter = require('./routes/auth.routes');
@@ -9,9 +10,12 @@ const messageRouter = require('./routes/message.routes');
 const userRouter = require('./routes/user.routes');
 const connectDB = require('./config/db.config');
 const errorHandler = require('./utils/errorHandler');
-dotenv.config();
 
 const PORT = process.env.PORT || 8080;
+
+__dirname = path.resolve();
+
+dotenv.config();
 
 // To read data from frontend or an external client
 app.use(bodyParser.json());
@@ -24,6 +28,12 @@ app.use(cookieParser());
 app.use('/api/auth', authRouter);
 app.use('/api/messages', messageRouter);
 app.use('/api/users', userRouter);
+
+app.use(express.static(path.join(__dirname, `/frontend/dist`)));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 app.use(errorHandler); // Last middleware to be called for error handling
 
